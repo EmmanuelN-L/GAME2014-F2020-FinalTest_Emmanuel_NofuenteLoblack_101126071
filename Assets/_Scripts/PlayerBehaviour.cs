50354afb2fdb5a30b16f27a2c482c7f0fca8eea4
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2020-12-19 14:56:31 -0500

    Added new shrinking platform controller script and created a shrinking platform
    Edited functionality in the player behaviour to account for the shrinking platform.

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -282,6 +282,12 @@ public class PlayerBehaviour : MonoBehaviour
             other.gameObject.GetComponent<MovingPlatformController>().isActive = true;
             transform.SetParent(other.gameObject.transform);
         }
+
+        if (other.gameObject.CompareTag("Shrinking Platform"))
+        {
+            other.gameObject.GetComponent<ShrinkingPlatformController>().isActive = true;
+            transform.SetParent(other.gameObject.transform);
+        }
     }
 
     private void OnCollisionExit2D(Collision2D other)
@@ -289,6 +295,12 @@ public class PlayerBehaviour : MonoBehaviour
         if (other.gameObject.CompareTag("Moving Platform"))
         {
             other.gameObject.GetComponent<MovingPlatformController>().isActive = false;
+            transform.SetParent(parent);
+        }
+
+        if (other.gameObject.CompareTag("Shrinking Platform"))
+        {
+            other.gameObject.GetComponent<ShrinkingPlatformController>().isActive = false;
             transform.SetParent(parent);
         }
     }
